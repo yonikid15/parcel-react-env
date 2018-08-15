b3e1941e3b7f6428810f135b0c4d07853dd604f8
Yonathan <yoni.kid15@gmail.com>
2018-08-15 00:56:29 -0400

    first commit: trying out new parcel react env

---
+++ README.md
@@ -0,0 +1,17 @@
+# React Development Environment
+
+## Bundler
+
+- Parcel Bundler
+    - HMR
+    - Code Splitting
+    - Automatically looks for postcssrc. and babelrc.
+
+## Linters
+
+- Eslint 
+- Prettier
+
+## Post Pre-Processor 
+
+- PostCSS
---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styles from './styles/app.css';
+
+const App = () => (
+  <div>
+    <h1>PARCEL - REACT</h1>
+    <p>lets get started.</p>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
